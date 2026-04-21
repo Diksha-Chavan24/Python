654f1a6ea61fabbb3844832058a1de461622fa53
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-04-21 17:58:50 +0000

    Add files via upload

---
+++ program610.py
@@ -0,0 +1,19 @@
+def CheckDivisible(No):
+    if((No % 3 == 0) and (No % 5 == 0)):
+        return True
+    else:
+        return False
+
+def main():
+    print("Enter the number : ")
+    Value = int(input())
+
+    bRet = CheckDivisible(Value)
+
+    if(bRet == True):
+        print(f"{Value} is divisible by 3 & 5")
+    else:
+        print(f"{Value} is not divisible by 3 &(or) 5")
+
+if __name__ == "__main__":
+    main()
