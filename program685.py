449946f5ae7d598d704641f1803ed039bdc0036d
Diksha-Chavan24 <dikshaac2406@gmail.com>
2026-04-26 14:10:40 +0000

    Add files via upload

---
+++ program685.py
@@ -0,0 +1,26 @@
+# input row : 5 
+ 
+'''
+        *
+      *   *
+    *   *   *
+  *   *   *   *
+*   *   *   *   *   
+'''
+
+def Display(iRow,):
+    
+    for i in range(1, iRow+1, 1):
+        print(" "*(iRow-i),end="")
+        print("* "*i)
+
+    print()
+    
+def main():
+    print("Enter number of rows : ")
+    Value1 = int(input())
+
+    Display(Value1)
+        
+if __name__ == "__main__":
+    main()
---
+++ program686.py
@@ -0,0 +1,26 @@
+# input row : 5 
+ 
+'''
+*   *   *   *   * 
+  *   *   *   *
+    *   *   *
+      *   *
+        *  
+'''
+
+def Display(iRow,):
+    
+    for i in range(iRow, 0, -1):
+        print(" "*(iRow-i),end="")
+        print("* "*i)
+
+    print()
+    
+def main():
+    print("Enter number of rows : ")
+    Value1 = int(input())
+
+    Display(Value1)
+        
+if __name__ == "__main__":
+    main()
